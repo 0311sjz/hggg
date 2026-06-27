fd942f52de76aa9db1d60e84a80f74b4cd2b8951
0311sjz <0311sjz@users.noreply.github.com>
2026-06-27 22:51:55 +0800

    fix: use excludeMatches to distinguish ad page from playback page

--- src/apps/com.phoenix.read.ts
+++ src/apps/com.phoenix.read.ts
@@ -18,7 +18,7 @@ export default defineGkdApp({
     {
       key: 2,
       name: '功能类-自动[上滑]继续看短剧',
-      desc: '读秒结束后[上滑]广告页面跳过广告',
+      desc: '广告页读秒结束后自动上滑跳过',
       rules: [
         {
           fastQuery: true,
@@ -35,34 +35,8 @@ export default defineGkdApp({
           },
           activityIds:
             'com.dragon.read.component.shortvideo.impl.ShortSeriesActivity',
-          matches: '[text="上滑继续观看短剧"][visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/i/29092674',
-          excludeSnapshotUrls: 'https://i.gkd.li/i/29092652',
-          exampleUrls: 'https://e.gkd.li/36e57d51-d134-4507-9aca-ada9e71b9b14',
-        },
-      ],
-    },
-    {
-      key: 3,
-      name: '功能类-自动[上滑]继续看短剧(播放页)',
-      desc: '播放页底部进度条出现时自动上滑继续',
-      rules: [
-        {
-          fastQuery: true,
-          swipeArg: {
-            start: {
-              x: 'screenWidth/2',
-              y: 'screenHeight * 0.9',
-            },
-            end: {
-              x: 'screenWidth/2',
-              y: 'screenHeight * 0.5',
-            },
-            duration: 200,
-          },
-          activityIds:
-            'com.dragon.read.component.shortvideo.impl.ShortSeriesActivity',
-          matches: '[vid="hcg"][visibleToUser=true]',
+          anyMatches: ['[vid="ise"][visibleToUser=true]'],
+          excludeMatches: ['[vid="hrc"][visibleToUser=true]'],
         },
       ],
     },
